e53a2a449bf128988fdba796640f947181e5182a
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-03-23 22:38:32 +0530

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import Head from "next/head";
+import React, { useState } from "react";
+import { Nav, MobileNav } from "@/components/navigation";
+import Footer from "@/components/footer/Footer";
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	const [nav, setNav] = useState(false);
+	const openNav = () => setNav(true);
+	const closeNav = () => setNav(false);
+
+	return (
+		<div className='overflow-x-hidden'>
+			<Head>
+				<title>Maa Tarini Electrical</title>
+				<meta
+					name='description'
+					content='Maa Tarini Electrical is an electrical shop based in Cuttack, Odisha. We offer complete range of electrical services.'
+				/>
+			</Head>
+			<div>
+				<MobileNav nav={nav} closeNav={closeNav} />
+				<Nav openNav={openNav} />
+				<Footer />
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
