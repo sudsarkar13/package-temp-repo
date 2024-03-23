4c6b3c8d95b6f317d1d076eb667efd23b706eff3
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-03-24 03:56:20 +0530

    u

---
+++ components/gallery/Gallery.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GCarousel from "./GCarousel";
+
+const Gallery = () => {
+	return <div className="gallery">
+    <div className="gcarousel">
+      <GCarousel />
+    </div>
+  </div>;
+};
+
+export default Gallery;
--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Gallery from "../gallery/Gallery";
 
 const Hero = () => {
-	return <div className="hero">Hero</div>;
+	return <div className="hero">
+    <div className="hero-container">
+      <Gallery />
+    </div>
+  </div>;
 };
 
 export default Hero;
